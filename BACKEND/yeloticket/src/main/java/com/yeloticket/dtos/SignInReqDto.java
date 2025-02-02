0b8c34e51563ddb5ed7a27ec4c9fcd80f3228345
ShashankSNUXS <shashanksharmamanu95555@gmail.com>
2025-02-02 21:30:36 +0530

    Extra files deleted

--- BACKEND/yeloticket/src/main/java/com/yeloticket/dtos/SignInReqDto.java
+++
@@ -1,9 +0,0 @@
-package com.yeloticket.dtos;
-
-import lombok.Data;
-
-@Data
-public class SignInReqDto {
-    private String username;
-    private String password;
-}
--- BACKEND/yeloticket/src/main/java/com/yeloticket/entities/UserRole.java
+++
@@ -1,5 +0,0 @@
-package com.yeloticket.entities;
-
-public enum UserRole {
-    USER,ADMIN
-}
